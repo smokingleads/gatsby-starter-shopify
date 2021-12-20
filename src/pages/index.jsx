9646e0e901e6b59d62994366fd589b884cb18d91
smokingleads <94363072+smokingleads@users.noreply.github.com>
2021-12-19 20:30:27 -0500

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -26,21 +26,8 @@ function Hero (props) {
       {!!process.env.GATSBY_DEMO_STORE && (
         <>
           <p className={callOut}>
-            It's a proof-of-concept in a box, with 10k products and 30k variants
-            to help you get to proof-of-concept as soon as right now.
-          </p>
-          <p className={callToAction}>
-            Hook it up to your own Shopify store data and start customizing in
-            minutes by deploying it to Gatsby Cloud for free. Grab your Shopify
-            store credentials and
-            <a href="https://www.gatsbyjs.com/dashboard/deploynow?url=https://github.com/gatsbyjs/gatsby-starter-shopify&utm_campaign=shopify-starter">
-              <img
-                src="https://www.gatsbyjs.com/deploynow.png"
-                alt="Deploy to Gatsby Cloud"
-                className={deployButton}
-              />
-            </a>
-          </p>
+            Precise and accurate leads for your business
+         
         </>
       )}
     </div>
